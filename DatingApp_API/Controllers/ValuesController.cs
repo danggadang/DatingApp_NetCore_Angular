bc782c75a185256e673105c1e08f78ea58594009
Dang Luong <lsd25897@gmail.com>
2020-09-10 18:28:45 +0700

    Initial Commit

---
+++ DatingApp_API/Controllers/ValuesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp_API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace DatingApp_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    //WeatherForecastController
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+        //IActionResult allow controller to return http response instead of just string
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            //FirstOrDefault return first value that it finds or returns NULL instead of Exception if it cant find any
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(value);
+        }
+    }
+}
---
+++ DatingApp_API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using DatingApp_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp_API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> option) : base(option) { }
+        public DbSet<Value> Values { get; set;}
+    }
+}
